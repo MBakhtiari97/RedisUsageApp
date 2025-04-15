771e7aa052d3773cfe1e68ae0a1bb9b20a47bbe3
Mohammad Bakhtiari <mohammadba86@yahoo.com>
2025-04-15 17:56:59 +0330

    Adding order item entity

---
+++ Core/OrderItem.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Core;
+
+public class OrderItem
+{
+    [Key]
+    [Required]
+    public long OrderItemId { get; set; }
+    [Required]
+    public long OrderId { get; set; }
+    [Required]
+    [StringLength(250)]
+    public string ItemName { get; set; } = string.Empty;
+
+    #region Navigation Properties
+
+    [JsonIgnore]
+    public Order Order { get; set; }
+
+    #endregion
+}
